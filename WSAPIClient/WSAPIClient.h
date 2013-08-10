266817046039694014a7f9386efa06c0ab81eff0
Stefan Church <stefan.church@gmail.com>
2013-08-10 14:55:22 +1200

    Initial Commit

---
+++ WSAPIClient/WSAPIClient.h
@@ -0,0 +1,13 @@
+//
+//  WSAPIClient.h
+//  WSAPIClient
+//
+//  Created by Stefan Church on 10/08/2013.
+//  Copyright (c) 2013 Stefan Church. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WSAPIClient : NSObject
+
+@end
---
+++ WSAPIClient/WSAPIClient.m
@@ -0,0 +1,13 @@
+//
+//  WSAPIClient.m
+//  WSAPIClient
+//
+//  Created by Stefan Church on 10/08/2013.
+//  Copyright (c) 2013 Stefan Church. All rights reserved.
+//
+
+#import "WSAPIClient.h"
+
+@implementation WSAPIClient
+
+@end
